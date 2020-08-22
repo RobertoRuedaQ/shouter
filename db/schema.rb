9660698d7567f8f4c02bd03b7582ad856d36763e
roberto rueda <rueda.roberto89@gmail.com>
2020-08-22 16:37:01 -0500

    Add photo shout with paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_215353) do
+ActiveRecord::Schema.define(version: 2020_08_22_194604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photo_shouts", force: :cascade do |t|
+    t.string "image_file_name", null: false
+    t.string "image_content_type", null: false
+    t.bigint "image_file_size", null: false
+    t.datetime "image_updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "shouts", force: :cascade do |t|
     t.bigint "user_id"
